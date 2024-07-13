146f7734a3401c4c09840e8a17df26326d928fff
aprizalabyan <abi@jkt1.ebdesk.com>
2024-07-13 21:37:09 +0700

    add form new-user, btn edit & delete

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -3,10 +3,17 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import UserList from "@/components/UserList"
+import ButtonDefault from "@/components/ButtonDefault"
+import InputTextDefault from '@/components/InputTextDefault'
+import { UserIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 
 export default function Home() {
   const [listUser, setListUser] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [showForm, setShowForm] = useState(false)
+  const [formType, setFromType] = useState('add')
 
   async function getAllUsers() {
     await axios.get('/api/user')
@@ -15,17 +22,55 @@ export default function Home() {
       })
   }
 
+  async function addUser(e: React.FormEvent) {
+    e.preventDefault();
+    console.log({ name, email });
+  };
+
   useEffect(() => {
     getAllUsers();
   }, [])
+
+  useEffect(() => {
+    setName('')
+    setEmail('')
+  }, [showForm])
 
   return (
     <div className="flex flex-col">
       <span>Home Page</span>
       <br />
-      <UserList listUser={listUser} />
+      <div className="flex gap-6">
+        <UserList listUser={listUser} formType={(e) => setFromType(e)} onClickAdd={(e) => setShowForm(e)} />
+        {showForm &&
+          <form onSubmit={addUser} className="space-y-2">
+            <span>{formType == 'add' ? 'Add New' : 'Edit'} User</span>
+            <InputTextDefault
+              label='Name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              prevendIcon={UserIcon}
+              required
+            />
+            <InputTextDefault
+              label='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              prevendIcon={EnvelopeIcon}
+              required
+            />
+            <ButtonDefault
+              type='submit' 
+              onClick={() => addUser} 
+              className='bg-gray-600 text-gray-100 min-w-20 text-sm'
+            >
+              {formType == 'add' ? 'Add' : 'Save'}
+            </ButtonDefault>
+          </form>
+        }
+      </div>
       <br />
-      <Link href={"/about"}>about page</Link>
+      <Link className='w-fit underline' href={"/about"}>about page</Link>
     </div>
   );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import React, { useEffect, useState } from 'react';
+import ButtonDefault from './ButtonDefault';
+import { PencilIcon, TrashIcon, PlusIcon, XMarkIcon } from '@heroicons/react/24/solid';
+
 interface User {
   _id: string,
   name: string,
@@ -7,31 +11,68 @@ interface User {
 }
 
 interface Props {
-  listUser: User[]
+  listUser: User[];
+  formType: (data: string) => void;
+  onClickAdd: (data: boolean) => void;
 }
 
-const UserList: React.FC<Props> = ({ listUser }) => {
+const UserList: React.FC<Props> = ({ listUser, formType, onClickAdd }) => {
+  const [isShowForm, setIsShowForm] = useState(false)
+
+  const handleEdit = (id: string) => {
+    console.log('cliicck edit', id);
+    formType('edit')
+    setIsShowForm(true);
+  };
+
+  const handleDelete = (id: string) => {
+    console.log('cliicck del', id);
+  };
+
+  const f_onClickAdd = () => {
+    formType('add')
+    setIsShowForm(!isShowForm);
+  }
+
+  useEffect(() => {
+    onClickAdd(isShowForm);
+  }, [isShowForm])
+
   return (
     <div>
-      <span>User List</span>
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="border-2 border-gray-400 min-w-40">Name</th>
-              <th className="border-2 border-gray-400 min-w-60">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {listUser.map((item) => (
-              <tr key={item._id}>
-                <td className="px-2 border-2 border-gray-400">{item.name}</td>
-                <td className="px-2 border-2 border-gray-400">{item.email}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className='flex justify-between mb-2'>
+        <span>User List</span>
+        <ButtonDefault
+          onClick={f_onClickAdd}
+          icon={isShowForm ? XMarkIcon : PlusIcon}
+          className={`${isShowForm ? 'bg-gray-300 text-gray-500' : 'bg-gray-600 text-gray-100'}  text-sm`}
+        >
+          {isShowForm ? 'Cancel' : 'Add New'}
+        </ButtonDefault>
       </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="border-2 border-gray-400 min-w-40">Name</th>
+            <th className="border-2 border-gray-400 min-w-60">Email</th>
+            <th className="border-2 border-gray-400 min-w-32">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUser.map((item) => (
+            <tr key={item._id}>
+              <td className="px-2 border-2 border-gray-400">{item.name}</td>
+              <td className="px-2 border-2 border-gray-400">{item.email}</td>
+              <td className="px-2 border-2 border-gray-400">
+                <div className='flex justify-center gap-2 py-1'>
+                  <ButtonDefault onClick={() => handleEdit(item._id)} icon={PencilIcon} className='bg-gray-300 text-gray-500' />
+                  <ButtonDefault onClick={() => handleDelete(item._id)} icon={TrashIcon} className='bg-gray-300 text-gray-500' />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
